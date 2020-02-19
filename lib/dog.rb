06498c9c7f7cd8084694431d489450afd46df2b4
Christopher Leja <christopher.leja@gmail.com>
2020-02-19 18:57:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,12 +5,20 @@ class Dog
     @breed = breed
   end
   
-  def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
   
   def name
     @name 
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    @breed
   end
   
 end
