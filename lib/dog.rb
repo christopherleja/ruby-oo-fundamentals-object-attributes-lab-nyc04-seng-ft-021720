7cbcd09ffb731fd77b44ccdc3b1a7d8bea376a61
Christopher Leja <christopher.leja@gmail.com>
2020-02-19 18:58:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,16 @@
 class Dog
   
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
   
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
   
   def name
     @name 
   end
   
-  def breed=(new_breed)
-    @breed = new_breed
+  def breed=(breed)
+    @breed = breed
   end
   
   def breed
